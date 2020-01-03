759acd011c1dc8aff6674e67e77444e8b8f4879c
wssiagt <57601508+wssiagt@users.noreply.github.com>
2020-01-03 08:47:39 +0800

    Update 实盘代码.py

--- 实盘代码.py
+++ 实盘代码.py
@@ -1,5 +1,5 @@
 import time
-k = -0.01
+k = -0.05
 new_amount = 0
 new_amount1 = 0
 new_profit = 0
@@ -70,15 +70,20 @@ def get_orders(buyprice, inibid, sellprice, iniask, profitrate):
     orders = exchange.GetOrders()
     for x in orders:
         if x.Id:
-            if x.Offset == 1 and x.Price == iniask and current_profitrate < -0.01:
-                return 1
-            elif x.Offset == 1 and x.Price == iniask and current_profitrate + 0.01 < -0.02:
+            Log(current_profitrate, current_id)
+            if x.Offset == 1 and x.Price == iniask and current_profitrate < -0.05:
+                while volum(iniamount) == update_position(new_amount):
+                    return 1
+            elif x.Offset == 1 and x.Price == iniask and current_profitrate - k < -0.02:
                 exchange.CancelOrder(x.Id)
-                return 2
-    if current_profitrate > -0.01:
-        return 3
-    elif current_profitrate < -0.01:
-        return 4
+                while volum(iniamount) != update_position(new_amount):
+                    return 2
+    if current_profitrate > -0.05:
+        while volum(iniamount) != update_position(new_amount):
+            return 3
+    elif current_profitrate < -0.05:
+        while volum(iniamount) != update_position(new_amount):
+            return 4
 
 def diff_condition(amount, iniamount, profitrate):
     iniask, inibid, buyprice, sellprice, tradeamount = depth(iniamount)
@@ -98,14 +103,15 @@ def diff_condition(amount, iniamount, profitrate):
 
 def main():
     Log('策略运行成功，', '开始监控')
+    iniamount = volum()
+    Log('初始仓位:', str(iniamount))
     while 1:
-        iniamount = volum()
         profitrate = initial()
         if profitrate < k:
-            Log('亏损率大于5%', '初始仓位：'+ str(iniamount), '开始交易')
+            Log('亏损率大于5%, 开始交易')
             iniask, inibid, buyprice, sellprice, tradeamount = depth(iniamount)
             closebuy(iniask, tradeamount)
-            Log('平仓挂单成功，开始getorders')
+            Log('平仓挂单成功，get orders')
             temp_var = get_orders(buyprice, inibid, sellprice, iniask, profitrate)
             while temp_var != 4:
                 Log('return:', temp_var)
@@ -113,15 +119,16 @@ def main():
                     #执行第二步
                     iniask, inibid, buyprice, sellprice, tradeamount = depth(iniamount)
                     closebuy(iniask, tradeamount)
-                    temp_var = get_orders(buyprice, inibid, sellprice, iniask, profitrate)
-                    time.sleep(0.2)
+                    while volum(iniamount) == update_position(new_amount):
+                        temp_var = get_orders(buyprice, inibid, sellprice, iniask, profitrate)
+                        time.sleep(0.1)
                 elif temp_var == 1:
-                    #执行第四步
                     temp_var = get_orders(buyprice, inibid, sellprice, iniask, profitrate)
-                    time.sleep(0.2)
+                    time.sleep(0.1)
                 elif temp_var == 3:
                     newo = exchange.GetOrders()
                     new_Id = newo[0].Id
+                    time.sleep(0.2)
                     exchange.CancelOrder(new_Id)
                     iniask, inibid, buyprice, sellprice, tradeamount = depth(iniamount)
                     temp_var = get_orders(buyprice, inibid, sellprice, iniask, profitrate)
@@ -129,7 +136,7 @@ def main():
                     Log('收益率相同')
                     time.sleep(0.2)
             Log('平仓已成交')
-            buytrade(buyprice,tradeamount)														# 否则，不相等（平仓成交）：
+            buytrade(buyprice,tradeamount)
             Log('开仓已挂单')
             new_amount, new_amount1, new_profitrate = update_position()
             Log('挂单已成交，卖出价：'+ str(iniask), '买入价：'+ str(buyprice), '成交量：'+ str(tradeamount), '当前持仓量: '+ str(new_amount1))	# 输出'成功'
